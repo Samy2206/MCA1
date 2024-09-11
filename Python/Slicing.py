2d1cf1f4ae58e9ecf3fcf9e9eb781127ea81f383
Samy2206 <sanketrajuadhav@gmail.com>
2024-09-11 23:46:56 +0530

    Changes in Slicing file saved

--- Python/Slicing.py
+++ Python/Slicing.py
@@ -4,3 +4,6 @@ data = arr.array('i',[10,20,30,40,50,60,70,80,90,100])
 print(data[2:6])    #slicing -> data[Start:End-1]
 print(data[0:10:3]) #slicing with jump 3 -> data[Start:End-1:Jump]
 print(data[::-1])   #To Reverse String 
+
+
+#done
