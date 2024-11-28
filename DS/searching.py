551776c257e9bb0ac2e9e44862ec82f0d372f0f2
Samy2206 <sanketrajuadhav@gmail.com>
2024-11-28 23:15:39 +0530

    Searching Updated

--- DS/searching.py
+++ DS/searching.py
@@ -1,30 +1,42 @@
 def linear_search(array,key):
-    if key in array:
-        for i in range(len(array)):
-            if array[i]==key:
-                print("Element Found at: ",i,"Position")
-    else:
-        print("Element is not present in array")
+    for i in range(len(array)):
+        if array[i]==key:
+            return("Element Found at: ",i,"Position")
+    return("Element is not present in array")
 
 
 def binary_search(array,key):
-    if key in array:
         array=sorted(array)
-        print(array)
         LB=0
         UB=len(array)-1
-        mid = (LB+UB)//2
-        while array[mid]!=key:
-            if key>array[mid]:
-                LB=mid+1
-                mid = (LB+UB)//2
-            else:
-                UB=mid-1
-                mid = (LB+UB)//2
-        print("Element is present at: ",mid," Position")
-    else:
-        print("Element is not present in array")
+        while LB<=UB:
+             mid = (LB+UB)//2
+             if array[mid]==key:
+                  return("Element is at:",mid,"Position")
+             elif array[mid]>key:
+                  UB=mid-1
+             else:
+                  LB=mid+1
+        return("Element not found")
+
+def search_rec(data,key,l,h):
+     if l > h:  # Base case for when the element is not found
+        return "Element not found"
+     mid=(l+h)//2
+     if data[mid]==key:
+          return ("Element is at:",mid,"Position")
+     elif data[mid]>key:
+          return search_rec(data,key,l,mid-1)
+     elif data[mid]<key:
+          return search_rec(data,key,mid+1,h)
+
+def bin_search_rec(data,key):
+     data=sorted(data)
+     l=0
+     h=len(data)-1
+     return search_rec(data,key,l,h)
 
 data = [10,20,50,30,60,70]
-linear_search(data,50)
-binary_search(data,50)
+print(linear_search(data,50))
+print(binary_search(data,20))
+print(bin_search_rec(data,50))
