4395cffb4b60ed82c05d5021d66427875117e7da
Samy2206 <sanketrajuadhav@gmail.com>
2024-12-22 22:35:32 +0530

    list functions

--- Practics/demo.py
+++ Practics/demo.py
@@ -37,3 +37,61 @@
 
 # for i in range(1,11):
 #     print(i,end=",")
+
+
+#dictionary
+# key : value
+
+data = {'name':'Avani',
+        'age':'40',
+        'marks':'90'
+        }
+    
+# for key,value in data.items():
+#     print(key,"-->",value)
+
+# val = data.values()
+# print(val)
+
+# key = data.keys()
+# print(key)
+
+# key = ['a','b','c','d']
+# val = [1,2,3,4]
+
+# res = {}
+
+# # res["key"]="val"
+
+
+# for i in range(len(key)):
+#     res[key[i]]=val[i]
+
+# print(res)
+
+
+data = [1,2,3,4]   #create a list
+print(data)
+data.append(5)
+data.append(6)
+print(data)     #append
+
+list2 = [7,8,9,10]
+data.extend(list2)
+print(data)
+
+data[4]=100
+print(data)         #update
+
+data.insert(4,200)
+print(data)         #insert
+
+print(data.pop())
+print(data)
+
+data.remove(1)         #remove
+print(data)
+
+print(len(data))          #length
+
+print(data.count(4))
---
+++ Practics/dict.py
@@ -0,0 +1,33 @@
+# key = ['a','b','c','d']
+# val = [1,2,3,4]
+# res={}
+# for i in range(len(key)):
+#     res[key[i]]=val[i]
+# print(res)
+
+#write a program to square the number take input from user
+# def square(num):
+#     return num*num
+
+# num=int(input("Enter  number"))
+# result=square(num)
+# print(result)
+
+#write program to check if the number is even or not using function
+# def number(num):
+#     if num%2==0:
+#         print("Number ",num,"is Even")
+#     else:
+#         print("Number ",num,"is odd")
+# number(40)
+
+
+# x=lambda num:num*num
+# print(x(23))
+
+# x=lambda num: "no is Even" if num%2==0 else "no is odd"
+# print(x(13))
+
+#homework
+#use in reduce . map and filter using lambda function
+
